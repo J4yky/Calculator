6ed8182afca1ad2179fcd5e13c43302d3bf76458
Jakub Naumowicz <jakub.oliwer@gmail.com>
2024-10-21 00:49:14 +0200

    Funkcje kwadratowe

--- src/main/java/com/Kalkulator/Main.java
+++ src/main/java/com/Kalkulator/Main.java
@@ -1,3 +1,5 @@
+package com.Kalkulator;
+
 public class Main {
     public static void main(String[] args) {
         new Project();
--- src/main/java/com/Kalkulator/Project.java
+++ src/main/java/com/Kalkulator/Project.java
@@ -1,3 +1,5 @@
+package com.Kalkulator;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -21,15 +23,18 @@ public class Project implements ActionListener {
     JButton[] mathSigns = new JButton[14];
     JButton plusSign, minusSign, multiplicationSign, divisionSign,
             dotSign, equalsSign, deleteSign, clearSign, squareRootSign, squareSign,
-            sineSign, cosineSign, tangentSign, percentageSign;
-    JPanel panel;
+            sineSign, cosineSign, tangentSign, percentageSign, fx;
+    JPanel panel, fxPanel, coordinatesPanel;
     ChartPanel chartPanel;
     ArrayList<Double> presentNumbers = new ArrayList<>();
     ArrayList<Character> presentOperators = new ArrayList<>();
     Font font1 = new Font("SansSerif", Font.BOLD, 50), font2 = new Font("SansSerif", Font.BOLD, 20);
-    double num1 = 0, finalValue = 0;
+    double num1 = 0, finalValue = 0, x1, x2, p, q, delta;
+    boolean x2IsPresent = false;
     String function = "";
     Dimension defaultFrameSize;
+    JLabel coordinateLabel, fxInstruction, x1CoordinateLabel, x2CoordinateLabel, vertexCoordinateLabel;
+
     Project(){
         frame = new JFrame("KalKULAtor");
         frame.setSize(600,600);
@@ -57,7 +62,6 @@ public class Project implements ActionListener {
         cosineSign = new JButton("cos()");
         tangentSign = new JButton("tan()");
 
-
         mathSigns[0] = plusSign;
         mathSigns[1] = minusSign;
         mathSigns[2] = multiplicationSign;
@@ -80,7 +84,7 @@ public class Project implements ActionListener {
             mathSigns[i].setFont(font2);
         }
 
-        for(int i = 0; i < 8; i++){                                         //kolorowanie przycisków
+        for(int i = 0; i < 8; i++){
             mathSigns[i].setBackground(Color.LIGHT_GRAY);
         }
 
@@ -99,7 +103,19 @@ public class Project implements ActionListener {
             numbers[i].setFont(font2);
         }
 
-        panel = new JPanel();                                               //panel z przyciskami
+        fx = new JButton("f(x)");
+        fx.addActionListener(this);
+        fx.setFocusable(false);
+        fx.setFont(font2);
+
+        fxInstruction = new JLabel("1. a   2. b   3. c   4. f(x)");
+        fxInstruction.setFont(font2);
+
+        fxPanel = new JPanel(new BorderLayout());
+        fxPanel.add(fxInstruction, BorderLayout.NORTH);
+        fxPanel.add(fx, BorderLayout.WEST);
+
+        panel = new JPanel();
         panel.setLayout(new GridLayout(7,4,10,10));
 
         panel.add(sineSign);
@@ -135,38 +151,111 @@ public class Project implements ActionListener {
 
         frame.add(textField, BorderLayout.NORTH);
         frame.add(panel, BorderLayout.CENTER);
+        frame.add(fxPanel, BorderLayout.SOUTH);
         frame.add(chartPanel, BorderLayout.EAST);
         frame.setVisible(true);
     }
 
-    public void createChart(double usersNumber, double calculatedPoint, String function){
+    public void createChart(double usersNumber, double calculatedPoint, String function, double a, double b, double c){
         XYSeriesCollection dataset = new XYSeriesCollection();
         XYSeries series = new XYSeries(function + "(x)");
 
-        for(double x = usersNumber -2 * Math.PI; x <= usersNumber + 2 * Math.PI; x += 0.1){
-            switch(function) {
-                case "sin":
-                    series.add(x, Math.sin(x));
-                    break;
-                case "cos":
-                    series.add(x, Math.cos(x));
-                    break;
-                case "tan":
-                    if (Math.abs(Math.cos(x)) > 0.0001) {
-                        series.add(x, Math.tan(x));
-                    }
-                    else{
-                        dataset.addSeries(series);
-                        series = new XYSeries(function + "(x)");
-                    }
-                    break;
+        if(a == 0 && b == 0 && c == 0) {
+            for (double x = usersNumber - 2 * Math.PI; x <= usersNumber + 2 * Math.PI; x += 0.01) {
+                switch (function) {
+                    case "sin":
+                        series.add(x, Math.sin(x));
+                        break;
+                    case "cos":
+                        series.add(x, Math.cos(x));
+                        break;
+                    case "tan":
+                        if (Math.abs(Math.cos(x)) > 0.0001) {
+                            series.add(x, Math.tan(x));
+                        } else {
+                            dataset.addSeries(series);
+                            series = new XYSeries(function + "(x)");
+                        }
+                        break;
+                }
             }
+        }
+        XYSeries rootPoints1 = new XYSeries("x0_1");
+        XYSeries rootPoints2 = new XYSeries("x0_2");
+        XYSeries vertex = new XYSeries("W(p,q)");
+
+        if(function.equals("quadratic") && !(a == 0)){
+            delta = b * b - (4 * a * c);
+            p = -b / (2 * a);
+            q = -delta / (4 * a);
+            if(delta > 0){
+                x2IsPresent = true;
+                x1 = (-b - Math.sqrt(delta)) / (2 * a);
+                x2 = (-b + Math.sqrt(delta)) / (2 * a);
+                rootPoints1.add(x1,0);
+                rootPoints2.add(x2,0);
+
+                for(double x = p - x1 - 2; x <= p + x2 + 2; x += 0.1) {
+                    series.add(x, a * x * x + b * x + c);
+                }
+            }
+            else if(delta == 0){
+                x1 = -b /(2 * a);
+                rootPoints1.add(x1,0);
+
+                for(double x = p - x1 - 2; x <= p + x1 + 2; x += 0.1) {
+                    series.add(x, a * x * x + b * x + c);
+                }
+            }
+            else {
+                for (double x = p - 10; x <= p + 10; x += 0.1) {
+                    series.add(x, a * x * x + b * x + c);
+                }
+            }
+
+            vertex.add(p,q);
         }
 
         XYSeries highlightedPointSeries = new XYSeries("Obliczony punkt");
         highlightedPointSeries.add(usersNumber, calculatedPoint);
         dataset.addSeries(series);
+        dataset.addSeries(rootPoints1);
+        dataset.addSeries(rootPoints2);
+        dataset.addSeries(vertex);
         dataset.addSeries(highlightedPointSeries);
+
+        if (coordinateLabel != null || x1CoordinateLabel != null ||
+                x2CoordinateLabel != null || vertexCoordinateLabel != null) {
+            chartPanel.remove(coordinateLabel);
+            chartPanel.remove(coordinatesPanel);
+            coordinatesPanel.removeAll();
+        }
+
+        coordinatesPanel = new JPanel(new FlowLayout());
+
+        coordinateLabel = new JLabel("");
+        coordinateLabel.setText("(" + String.format("%.2f", usersNumber) + "; " +
+                String.format("%.2f", calculatedPoint) + ")");
+
+        x1CoordinateLabel = new JLabel("");
+        if(delta >= 0){
+            x1CoordinateLabel.setText("M01: (" + String.format("%.2f", x1) + "; " +
+                    String.format("%.2f", 0.00) + ") ");
+        } else{
+            x1CoordinateLabel.setText("M01: BRAK ");
+        }
+
+        x2CoordinateLabel = new JLabel("");
+        if(delta > 0) {
+            x2CoordinateLabel.setText("M02(" + String.format("%.2f", x2) + "; " +
+                    String.format("%.2f", 0.00) + ") ");
+        } else{
+            x2CoordinateLabel.setText("M02: BRAK ");
+        }
+
+        vertexCoordinateLabel = new JLabel("");
+        vertexCoordinateLabel.setText("W(p,q): (" + String.format("%.2f", p) + "; " +
+                String.format("%.2f", q) + ")");
 
         JFreeChart chart = ChartFactory.createXYLineChart(
                 "Wykres funkcji " + function + "(x)",
@@ -175,9 +264,31 @@ public class Project implements ActionListener {
         );
 
         XYPlot plot = chart.getXYPlot();
-        plot.getDomainAxis().setRange(usersNumber - 2 * Math.PI, usersNumber + 2 * Math.PI);
-        if(function.equals("tan")) {
-            plot.getRangeAxis().setRange(-10, 10);
+        if(function.equals("quadratic")){
+            if(a > 0){
+                if(delta > 0) {
+                    plot.getDomainAxis().setRange(p - x1 - 2, p + x2 + 2);
+                    plot.getRangeAxis().setRange(q - 1, 2);
+                } else{
+                    plot.getDomainAxis().setRange(p - 10, p + 10);
+                    plot.getRangeAxis().setRange(q - 1, q + 10);
+                }
+            }
+            if(a < 0){
+                if(delta >0) {
+                    plot.getDomainAxis().setRange(p - x1 - 2, p + x2 + 2);
+                    plot.getRangeAxis().setRange(-2, q + 1);
+                } else {
+                    plot.getDomainAxis().setRange(p - 10, p + 10);
+                    plot.getRangeAxis().setRange(q - 10, q + 1);
+                }
+            }
+        }
+        else {
+            plot.getDomainAxis().setRange(usersNumber - 2 * Math.PI, usersNumber + 2 * Math.PI);
+            if (function.equals("tan")) {
+                plot.getRangeAxis().setRange(-10, 10);
+            }
         }
 
         ValueMarker yMarker = new ValueMarker(0);
@@ -185,13 +296,21 @@ public class Project implements ActionListener {
         ValueMarker xMarker = new ValueMarker(0);
         xMarker.setPaint(Color.blue);
 
-        ValueMarker yResultMarker = new ValueMarker(calculatedPoint);
-        ValueMarker xResultMarker = new ValueMarker(usersNumber);
+        if(!function.equals("quadratic")) {
+            ValueMarker yResultMarker = new ValueMarker(calculatedPoint);
+            ValueMarker xResultMarker = new ValueMarker(usersNumber);
+            plot.addRangeMarker(yResultMarker);
+            plot.addDomainMarker(xResultMarker);
+        } else{
+            ValueMarker qResultMarker = new ValueMarker(q);
+            ValueMarker pResultMarker = new ValueMarker(p);
+            plot.addRangeMarker(qResultMarker);
+            plot.addDomainMarker(pResultMarker);
+        }
 
         plot.addRangeMarker(yMarker);
         plot.addDomainMarker(xMarker);
-        plot.addRangeMarker(yResultMarker);
-        plot.addDomainMarker(xResultMarker);
+
 
         XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
 
@@ -199,9 +318,25 @@ public class Project implements ActionListener {
         renderer.setSeriesShapesVisible(0, false);
         renderer.setSeriesLinesVisible(1,false);
         renderer.setSeriesShapesVisible(1, true);
+        renderer.setSeriesLinesVisible(2,false);
+        renderer.setSeriesShapesVisible(2, true);
+        renderer.setSeriesLinesVisible(3,false);
+        renderer.setSeriesShapesVisible(3, true);
+        renderer.setSeriesLinesVisible(4,false);
+        renderer.setSeriesShapesVisible(4, true);
 
         plot.setRenderer(renderer);
 
+        if(function.equals("quadratic")) {
+            coordinatesPanel.add(x1CoordinateLabel);
+            coordinatesPanel.add(x2CoordinateLabel);
+            coordinatesPanel.add(vertexCoordinateLabel);
+            coordinatesPanel.revalidate();
+            coordinatesPanel.repaint();
+            chartPanel.add(coordinatesPanel, BorderLayout.SOUTH);
+        } else {
+            chartPanel.add(coordinateLabel, BorderLayout.SOUTH);
+        }
         chartPanel.setChart(chart);
         chartPanel.setVisible(true);
         frame.pack();
@@ -210,22 +345,27 @@ public class Project implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e){
-        for(int i = 0; i < 10; i++){                                     //obsluga przyciskow z cyframi
+        for(int i = 0; i < 10; i++){
             if(e.getSource() == numbers[i]){
                 textField.setText(textField.getText().concat(String.valueOf(i)));
             }
         }
 
-        if(e.getSource() == plusSign){                                  //obsluga przysiskow z operacjami
+        if(e.getSource() == plusSign){
             presentNumbers.add(Double.parseDouble(textField.getText()));
             presentOperators.add('+');
             textField.setText("");
         }
 
         else if(e.getSource() == minusSign){
-            presentNumbers.add(Double.parseDouble(textField.getText()));
-            presentOperators.add('-');
-            textField.setText("");
+            if(textField.getText().isEmpty()){
+                textField.setText("-");
+            }
+            else {
+                presentNumbers.add(Double.parseDouble(textField.getText()));
+                presentOperators.add('-');
+                textField.setText("");
+            }
         }
 
         else if(e.getSource() == multiplicationSign){
@@ -260,7 +400,7 @@ public class Project implements ActionListener {
             num1 = Math.toRadians(num1);
             finalValue = Math.sin(num1);
             textField.setText(String.valueOf(finalValue));
-            createChart(num1, finalValue,function);
+            createChart(num1, finalValue,function,0,0,0);
             finalValue = num1;
         }
 
@@ -270,7 +410,7 @@ public class Project implements ActionListener {
             num1 = Math.toRadians(num1);
             finalValue = Math.cos(num1);
             textField.setText(String.valueOf(finalValue));
-            createChart(num1, finalValue,function);
+            createChart(num1, finalValue,function,0,0,0);
             finalValue = num1;
         }
         else if(e.getSource() == tangentSign){
@@ -279,8 +419,34 @@ public class Project implements ActionListener {
             num1 = Math.toRadians(num1);
             finalValue = Math.tan(num1);
             textField.setText(String.valueOf(finalValue));
-            createChart(num1, finalValue,function);
+            createChart(num1, finalValue,function,0,0,0);
             finalValue = num1;
+        }
+        else if(e.getSource() == fx){
+            presentNumbers.add(Double.parseDouble(textField.getText()));
+            double a;
+            double b;
+            double c;
+            if(presentNumbers.size() == 3 && presentOperators.size() == 2) {
+                function = "quadratic";
+                a = presentNumbers.getFirst();
+                if(presentOperators.getFirst() == '-') {
+                    b = 0 - presentNumbers.get(1);
+                } else {
+                    b = presentNumbers.get(1);
+                }
+                if(presentOperators.getLast() == '-') {
+                    c = 0 - presentNumbers.getLast();
+                } else {
+                    c = presentNumbers.getLast();
+                }
+                if(!(a == 0)) {
+                    createChart(0, 0, function, a, b, c);
+                }
+            }
+            presentNumbers.clear();
+            presentOperators.clear();
+            finalValue = 0;
         }
         else if(e.getSource() == percentageSign){
             if(!presentNumbers.isEmpty()){
@@ -293,7 +459,7 @@ public class Project implements ActionListener {
             }
         }
 
-        else if(e.getSource() == dotSign){                          //obsluga kropki
+        else if(e.getSource() == dotSign){
             String scanner = textField.getText();
 
             if(scanner.contains(".")){
@@ -303,7 +469,7 @@ public class Project implements ActionListener {
             textField.setText(textField.getText().concat("."));
         }
 
-        else if(e.getSource() == equalsSign){                       //obsluga wyniku
+        else if(e.getSource() == equalsSign){
             if(presentNumbers.isEmpty()){
                 textField.setText("ERROR");
                 return;
@@ -349,7 +515,7 @@ public class Project implements ActionListener {
             presentOperators.clear();
         }
 
-        else if(e.getSource() == clearSign) {               //clear i delete
+        else if(e.getSource() == clearSign) {
             textField.setText("");
             finalValue = 0;
         }
